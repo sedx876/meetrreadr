01327b2c9926efacea3acb7440227c5142291834
sedx876 <swatkins876@gmail.com>
2020-03-06 07:21:49 -0500

    added tag model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_115828) do
+ActiveRecord::Schema.define(version: 2020_03_06_121804) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 2020_03_06_115828) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["book_id"], name: "index_comments_on_book_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
