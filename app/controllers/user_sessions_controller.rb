0fa750e79c1047743049675eeb78874242ebf132
sedx876 <swatkins876@gmail.com>
2020-03-10 14:48:17 -0400

    Code clean up

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,36 +6,26 @@ class UserSessionsController < ApplicationController
 
   def create
     if request.env["omniauth.auth"]
-        @user = User.find_by(github_uid: request.env["omniauth.auth"]["uid"])
+      @user = User.find_by(github_uid: request.env["omniauth.auth"]["uid"])
         if @user.nil?
-            @user = User.create(username: request.env["omniauth.auth"]["info"]["nickname"], github_uid: request.env["omniauth.auth"]["uid"], password: "github")
+          @user = User.create(username: request.env["omniauth.auth"]["info"]["nickname"], github_uid: request.env["omniauth.auth"]["uid"], password: "github")
         end
-        log_in(@user)
-        redirect_to books_path
-    else
-        @user = User.find_by(username: params[:username])
-        if !@user
-            #@error = "Account not found. Please try again."
-            render :new
-        elsif !@user.authenticate(params[:password])
-            #@error = "Password incorrect. Please try again."
-            render :new
-        else
-            log_in(@user)
-            redirect_to books_path
-        end
+          log_in(@user)
+          redirect_to books_path
+       else
+          @user = User.find_by(username: params[:username])
+       if !@user
+          flash.notice = "Account not found. Please try again."
+          render :new
+       elsif !@user.authenticate(params[:password])
+          flash.notice = "Password incorrect. Please try again."
+          render :new
+       else
+          log_in(@user)
+          redirect_to books_path
+       end
     end
-end
-
-  # def create
-  #   @user = User.find_by(username: params[:username])
-  #     if @user && @user.authenticate(params[:password])
-  #       session[:user_id] = @user.id
-  #       redirect_to books_path
-  #     else
-  #       render :new
-  #     end
-  # end
+  end
 
   def destroy
     session.clear
